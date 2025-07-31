77f4a16d09aabc6dccd8e66cc99e5476c5302696
VanshGarg208 <vanshgarg1782006@gmail.com>
2025-07-31 23:06:44 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (53.05%) - LeetSync

---
+++ 231-power-of-two/power-of-two.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        long long ans = 1;
+
+        for (int i = 0; i <= 30; i++) {
+
+            if (ans == n) {
+                return true;
+            }
+
+            ans = ans * 2;
+        }
+
+        return false;
+    }
+};
