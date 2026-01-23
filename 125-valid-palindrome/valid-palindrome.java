4235ef44fa4a81aa5be95a7a2eb171f3a57f4224
VanshGarg208 <vanshgarg1782006@gmail.com>
2026-01-23 15:17:41 +0530

    Time: 4 ms (51.51%) | Memory: 45.1 MB (43.06%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isPalindrome(String str) {
+        str = str.trim();
+        //String[] arr = str.split("//s+");
+        StringBuilder sb = new StringBuilder("");
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+
+            if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z') || (ch >= '0' && ch <= '9')) sb.append(Character.toLowerCase(ch));
+
+        }
+
+        String in = sb.toString();
+        String out = new StringBuilder(in).reverse().toString();
+
+        if (in.length() != out.length()) return false;
+        for (int i = 0; i < in.length(); i++) {
+            char t1 = in.charAt(i);
+            char t2 = out.charAt(i);
+
+            if (t1 != t2) return false;
+        }
+        return true;
+    }
+}
