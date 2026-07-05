aea7f55cf8fca4a8c3597315cc66b604ab16031c
VanshGarg208 <vanshgarg1782006@gmail.com>
2026-07-05 12:37:11 +0530

    Time: 0 ms (100%), Space: 43.5 MB (61.19%) - LeetHub

--- 0027-remove-element/0027-remove-element.java
+++ 0027-remove-element/0027-remove-element.java
@@ -1,15 +1,15 @@
 class Solution {
     public int removeElement(int[] nums, int val) {
-        int size = nums.length;
-        for (int i = 0; i < size; i++) {
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
             if (val == nums[i]) {
-                for (int j = i; j < size - 1; j++) {
+                for (int j = i; j < n-1; j++) {
                     nums[j] = nums[j+1];
                 }
-                size--;
                 i--;
+                n--;
             }
         }
-        return size;
+        return n;
     }
 }
